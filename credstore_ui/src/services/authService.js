1bc17e78f786f5340df9fc0956a65d6f41a27b61
YSS <yalavarthisainadh@gmail.com>
2024-03-07 21:04:34 +0530

    Update authService.js

--- credstore_ui/src/services/authService.js
+++ credstore_ui/src/services/authService.js
@@ -1,15 +1,20 @@
 import axios from "axios";
+
 const baseUrl =
   process.env.REACT_AUTH_SERVICE || "http://localhost:5219/api/Auth";
 
-const login = async (credentials) => {
-  const response = await axios.post(`${baseUrl}/login`, credentials);
-  return response;
+const authService = {
+  login: async function (credentials) {
+    const response = await axios.post(`${baseUrl}/login`, credentials);
+    return response;
+  },
+  register: async function (registrationRequest) {
+    const response = await axios.post(
+      `${baseUrl}/register`,
+      registrationRequest
+    );
+    return response;
+  },
 };
 
-const register = async (registrationRequest) => {
-  const response = await axios.post(`${baseUrl}/register`, registrationRequest);
-  return response;
-};
-
-export default { login, register };
+export default authService;
