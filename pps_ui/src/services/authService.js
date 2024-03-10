458b280585b8e55e94d17626c7be84f0d7c1ef76
YSS <yalavarthisainadh@gmail.com>
2024-03-10 21:11:50 +0530

    Update authService.js

--- pps_ui/src/services/authService.js
+++ pps_ui/src/services/authService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 const baseUrl =
-  process.env.REACT_AUTH_SERVICE || "http://localhost:5219/api/Auth";
+  //process.env.REACT_AUTH_SERVICE || "http://localhost:5219/api/Auth";
+ 
 
-//const baseUrl = `http://3.85.17.0:5000/api/Auth`;
+const baseUrl = `http://3.85.17.0:5000/api/Auth`;
 
 const login = async (credentials) => {
   const response = await axios.post(`${baseUrl}/login`, credentials);
